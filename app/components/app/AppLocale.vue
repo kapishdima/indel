fd88d03b7ac86d7ea40e64e6ed9e1eb2cf6a4fb9
dev_wandry <dima.development@wandry.com.ua>
2026-05-04 19:33:36 +0300

    feat: temp hidden applocale

--- app/components/app/AppLocale.vue
+++ app/components/app/AppLocale.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="header-locales">
-    <NuxtLink :to="$switchLocalePath('uk-UA')" class="header-locales__item">
+    <!-- <NuxtLink :to="$switchLocalePath('uk-UA')" class="header-locales__item">
       <img src="/images/flags/uk.svg" alt="Українська" />
     </NuxtLink>
     <NuxtLink :to="$switchLocalePath('en')" class="header-locales__item">
       <img src="/images/flags/en.svg" alt="English" />
-    </NuxtLink>
+    </NuxtLink> -->
   </div>
 </template>
 
